587312bb61f64a4bbbbc33bf2576708ecd52898c
lesmanadel <lesmanadel3@gmail.com>
2020-07-14 23:00:42 +0700

    add button to add post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -6,7 +6,8 @@ export const BlogProvider = ({ children }) => {
   const [blogPosts, setBlogPosts] = useState([]);
 
   const addBlogPost = () => {
-    setBlogPosts([...blogPosts, { title: 'Blog Post #${blogPosts.length + 1}'}]);
+    setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}
+    ]);
   }
 
   return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
